d89672254a4348c56674fd28554fb5ee82e153c7
Sara <sarah.abiidar@gmail.com>
2024-11-28 20:01:07 +0100

    write post type and show as a list in postlist page

--- vite-axios-router-hook-mui-login/src/pages/PostListPage.tsx
+++ vite-axios-router-hook-mui-login/src/pages/PostListPage.tsx
@@ -1,8 +1,28 @@
+import Post from "../types/post";
 import { useNavigate } from "react-router-dom";
 import { appRouter } from "../router/appRouter";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const PostListPage = () => {
   const navigate = useNavigate();
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const getList = () => {
+    //const result = axios.get("https://jsonplaceholder.typicode.com/posts");
+    //result.then((res) => {
+    //console.log(res.data);
+    //});
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((error) => console.log("an error occured:", error));
+  };
+  useEffect(() => {
+    getList();
+  }, []);
   const handleHomeButton = () => {
     //window.location.href = appRouter.HOME_PAGE; this ist vanila javascript, this has same result same next line! but we use there useNavigate!
     navigate(appRouter.HOME_PAGE, {
@@ -11,8 +31,19 @@ const PostListPage = () => {
   };
   return (
     <div>
-      Post List
+      <h2>Post List</h2>
       <button onClick={handleHomeButton}>Go To Home</button>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <strong>Post Id:</strong>
+            {post.id}
+            <br />
+            <strong>Post Titile:</strong>
+            {post.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
