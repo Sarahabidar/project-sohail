aff2c9756199264ed448dba6f80ccdd62ab1c07b
Sara <sarah.abiidar@gmail.com>
2024-11-28 14:20:44 +0100

    wirte notfoundpage and defaultpage for our mainrouter

--- vite-axios-router-hook-mui-login/src/router/MainRouter.tsx
+++ vite-axios-router-hook-mui-login/src/router/MainRouter.tsx
@@ -1,12 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import PostListPage from "../pages/PostListPage";
 import { appRouter } from "./appRouter";
 import MainLayout from "../layout/MainLayout";
+import NotFoundPage from "../pages/NotFoundPage";
 const MainRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<DefaultRoute />} />
+        <Route path="/*" element={<NotFoundPage />} />
         <Route element={<MainLayout />}>
           <Route path={appRouter.HOME_PAGE} element={<HomePage />} />
           <Route path={appRouter.POST_LIST_PAGE} element={<PostListPage />} />
@@ -15,5 +18,5 @@ const MainRouter = () => {
     </BrowserRouter>
   );
 };
-
+const DefaultRoute = () => <Navigate to={appRouter.HOME_PAGE} />; //we write this for default value just /
 export default MainRouter;
