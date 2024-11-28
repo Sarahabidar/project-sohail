e09f9fa802f05dc294c057c67595111b9c115484
Sara <sarah.abiidar@gmail.com>
2024-11-28 11:55:06 +0100

    add moduls css for mainlayout and stayles to navlink

--- vite-axios-router-hook-mui-login/src/layout/MainLayout.tsx
+++ vite-axios-router-hook-mui-login/src/layout/MainLayout.tsx
@@ -1,10 +1,16 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { appRouter } from "../router/appRouter";
+import styles from "./MainLayout.module.css";
 const MainLayout = () => {
   return (
     <div>
       <div>
-        <NavLink to={appRouter.HOME_PAGE}>Home</NavLink>
+        <NavLink to={appRouter.HOME_PAGE} className={styles.navLink}>
+          Home
+        </NavLink>
+        <NavLink to={appRouter.POST_LIST_PAGE} className={styles.navLink}>
+          Post List
+        </NavLink>
       </div>
       <Outlet />
     </div>
